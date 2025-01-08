23c558a5bd67750817971dcfb7de2d920529c0a6
abhinav-desh <abhinav.deshmukh@pramericalife.in>
2025-01-08 15:42:46 +0530

    add some commit

---
+++ design2025/src/Components/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Navbar = () => {
+  const navElement = ['HOME', 'PAGES', 'PORTFOLIO', 'BLOG', 'SHOP'];
+
+  return (
+    <div className="Navbar-container">
+      <div className="Navbar-content1">
+        <img src="https://tadao.qodeinteractive.com/" alt="Logo" />
+        <p
+          style={{
+            fontFamily: 'Khand, sans-serif',
+            textTransform: 'uppercase',
+            color: '#8e7861',
+            fontSize: '16px',
+            lineHeight: '1.25em',
+            fontWeight: 600,
+            letterSpacing: '.1em',
+            margin: '2px 0 0 20px',
+          }}
+        >
+          TADAO ARCHITECTURE
+        </p>
+      </div>
+
+      <div className="Navbar-content2" style={{ display: 'flex', justifyContent: 'space-between', width: '60%' }}>
+        {navElement.map((ele, index) => {
+          return (
+            <div
+              key={index}
+              className={`naveffect ${ele.toLowerCase()}`} 
+              style={{
+                color: '#8e7861',
+                cursor: 'pointer',
+                width: '20%',
+                borderRight: index !== navElement.length - 1 ? '1px solid #8e7861' : 'none', 
+                textAlign: 'center',
+                padding: '10px 0',
+                transition: 'all 0.3s ease',
+              }}
+            >
+              {ele}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
